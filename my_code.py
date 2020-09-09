85160a8636cab228d661fa263d297a6f9c228d64
mishikag <23mgupta@athenian.org>
2020-09-09 20:29:22 +0000

    finished my age code

--- my_code.py
+++ my_code.py
@@ -1,20 +1,21 @@
 # Complete the function below calculate your age after different numbers of years.
 
+
 def age_now(x):
     # My age now
-    print(f"I am currently { } years old.\n")
+    print(f"I am currently {x} years old.\n")
 
 def age_1(y):
     # My age next year
-    print(f"Next year I'll be { } years old.\n")
+    print(f"Next year I'll be {y + 1} years old.\n")
 
 def age_10(z):
     # My age in 10 years
-    print(f"In 10 years, I'll be { }!\n")
+    print(f"In 10 years, I'll be {z+10}!\n")
 
 def age_50(q):
     # My age in 50 years!
-    print(f"In 50 years, I'll be { }! Wow!\n")
+    print(f"In 50 years, I'll be {q+50}! Wow!\n")
 
 
 
@@ -28,6 +29,7 @@ if __name__ == '__main__':
     # below it to get the user's age as an input.
     '''
 
+    age = int(input("how old are you?"))
     age_now(age)   # run function age_now with argument age
     age_1(age)    # run function age_1 with argument age
     age_10(age)    # run function age_10 with argument age
